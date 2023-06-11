66f81ac877a023e752a7bdef0a1e7c17be8cc117
Ken Sasongko <ken.sasongko@gmail.com>
2023-06-11 11:57:45 +0700

    Initial commit

---
+++ test/cdk-merged-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkMergedApi from '../lib/cdk-merged-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-merged-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkMergedApi.CdkMergedApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
