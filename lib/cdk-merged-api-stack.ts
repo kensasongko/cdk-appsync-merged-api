884f4f9a685ea8a48b09c1fda72bdf722b153863
Ken Sasongko <ken.sasongko@gmail.com>
2023-06-11 22:45:46 +0700

    Add AppSync Merged API

--- lib/cdk-merged-api-stack.ts
+++ lib/cdk-merged-api-stack.ts
@@ -1,16 +1,82 @@
 import * as cdk from 'aws-cdk-lib';
+import { CfnGraphQLApi, CfnSourceApiAssociation, UserPoolDefaultAction } from 'aws-cdk-lib/aws-appsync';
+import { PolicyDocument, PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkMergedApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    
+    const api1Id = StringParameter.fromStringParameterAttributes(this, 'Api1IdStringParameter', {
+      parameterName: '/ken/appsync/api1/apiId',
+    }).stringValue;
 
-    // The code that defines your stack goes here
+    const api2Id = StringParameter.fromStringParameterAttributes(this, 'Api2IdStringParameter', {
+      parameterName: '/ken/appsync/api2/apiId',
+    }).stringValue;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkMergedApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const userPoolId = StringParameter.fromStringParameterAttributes(this, 'UserPoolIdStringParameter', {
+      parameterName: '/ken/cognito/dummy/userPoolId',
+    }).stringValue;
+
+    const mergedApiRole = new Role(this, 'MergedApiRole', {
+      assumedBy: new ServicePrincipal("appsync.amazonaws.com"),
+      inlinePolicies: {
+        "SourceGraphQL": new PolicyDocument({
+          statements: [
+            new PolicyStatement({
+              actions: ["appsync:SourceGraphQL"],
+              resources: [
+                `arn:aws:appsync:${props?.env?.region}:${props?.env?.account}:apis/${api1Id}`,
+                `arn:aws:appsync:${props?.env?.region}:${props?.env?.account}:apis/${api2Id}`,
+                `arn:aws:appsync:${props?.env?.region}:${props?.env?.account}:apis/${api1Id}/*`,
+                `arn:aws:appsync:${props?.env?.region}:${props?.env?.account}:apis/${api2Id}/*`,
+              ]
+            }),
+          ]
+        }),
+      },
+    });
+
+    const myMergedApi = new CfnGraphQLApi(this, 'MyMergedApi', {
+      authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+      name: 'MyMergedApi',
+
+      apiType: 'MERGED',
+      mergedApiExecutionRoleArn: mergedApiRole.roleArn,
+      ownerContact: 'Trogdor The Burninator',
+      userPoolConfig: {
+        awsRegion: 'us-east-1',
+        userPoolId: userPoolId,
+        defaultAction: UserPoolDefaultAction.ALLOW,
+      },
+      xrayEnabled: true,
+    });
+
+    new CfnSourceApiAssociation(this, 'Api1SourceApiAssociation', {
+      description: 'Api 1 Source API',
+      mergedApiIdentifier: myMergedApi.attrApiId,
+      sourceApiAssociationConfig: {
+        mergeType: 'AUTO_MERGE',
+      },
+      sourceApiIdentifier: api1Id,
+    });
+
+    new CfnSourceApiAssociation(this, 'Api2SourceApiAssociation', {
+      description: 'Api 2 Source API',
+      mergedApiIdentifier: myMergedApi.attrApiId,
+      sourceApiAssociationConfig: {
+        mergeType: 'AUTO_MERGE',
+      },
+      sourceApiIdentifier: api2Id,
+    });
+
+    mergedApiRole.addToPolicy(new PolicyStatement({
+      actions: ["appsync:StartSchemaMerge"],
+      resources: [
+        `arn:aws:appsync:${props?.env?.region}:${props?.env?.account}:apis/${myMergedApi.attrApiId}/sourceApiAssociations/*`,
+      ]
+    }));
   }
 }
